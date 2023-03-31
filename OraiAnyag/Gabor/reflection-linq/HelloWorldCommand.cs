e694c8d931781570197b93178a8bc41a0da87911
gabor.ruzsinszki <gabor.ruzsinszki@evosoft.com>
2023-03-31 08:13:59 +0200

    Reflection & LINQ

---
+++ OraiAnyag/Gabor/reflection-linq/HelloWorldCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace reflection_linq
+{
+    internal class HelloWorldCommand : ICommand
+    {
+        public string Name => "hello";
+
+        public void Execute()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ OraiAnyag/Gabor/reflection-linq/Program.cs
@@ -0,0 +1,38 @@
+﻿using reflection_linq;
+
+Type t = typeof(ICommand);
+
+var commandTypes = t.Assembly.GetTypes()
+    .Where(type => type.IsAssignableTo(t)
+    && !type.IsAbstract
+    && !type.IsInterface);
+
+var commandTable = commandTypes
+            .Select(type => (ICommand)Activator.CreateInstance(type))
+            .ToDictionary(command => command.Name, command => command);
+
+do
+{
+    Console.Write("enter command> ");
+    string? input = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(input))
+        continue;
+
+    if (commandTable.ContainsKey(input))
+    {
+        try
+        {
+            commandTable[input].Execute();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Unknown command");
+    }
+}
+while (true);
