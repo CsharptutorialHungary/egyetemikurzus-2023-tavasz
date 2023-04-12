35331480eda724a7cffcae67895582c826d18b5d
Pós László <h050706@stud.u-szeged.hu>
2023-04-12 14:57:52 +0200

    reading JSON, queries, Pokemon and PokemonCollection model

---
+++ h050706/Pokemon.cs
@@ -0,0 +1,137 @@
+﻿
+
+[Serializable]
+public sealed class Pokemon
+{
+    public int id { get; set; }
+    public string num { get; set; }
+    public string name { get; set; }
+    public string img { get; set; }
+    public List<string> type { get; set; }
+    public string height { get; set; }
+    public string weight { get; set; }
+    public string candy { get; set; }
+    public int candy_count { get; set; }
+    public string egg { get; set; }
+    public double spawn_chance { get; set; }
+    public double avg_spawns { get; set; }
+    public string spawn_time { get; set; }
+    public List<double>? multipliers { get; set; }
+    public List<string> weaknesses { get; set; }
+    public List<Evolution>? prev_evolution { get; set; }
+    public List<Evolution> next_evolution { get; set; }
+
+    public Pokemon(
+        int id,
+        string num,
+        string name,
+        string img,
+        List<string> type,
+        string height,
+        string weight,
+        string candy,
+        int candy_count,
+        string egg,
+        double spawn_chance,
+        double avg_spawns,
+        string spawn_time,
+        List<double>? multipliers,
+        List<string> weaknesses,
+        List<Evolution> prev_evolution,
+        List<Evolution> next_evolution)
+    {
+        this.id = id;
+        this.num = num;
+        this.name = name;
+        this.img = img;
+        this.type = type;
+        this.height = height;
+        this.weight = weight;
+        this.candy = candy;
+        this.candy_count = candy_count;
+        this.egg = egg;
+        this.spawn_chance = spawn_chance;
+        this.avg_spawns = avg_spawns;
+        this.multipliers = multipliers;
+        this.weaknesses = weaknesses;
+        this.prev_evolution = prev_evolution;
+        this.next_evolution = next_evolution;
+    }
+
+    public Pokemon()
+    {
+        prev_evolution = new List<Evolution>();
+        next_evolution = new List<Evolution>();
+    }
+
+    public override string ToString()
+    {
+        string result =
+            "id: " + id + "\n"
+            + "num: " + num + "\n"
+            + "name: " + name + "\n"
+            + "img: " + img + "\n"
+            + "type:\n";
+        foreach (string element in type)
+        {
+            result += "\t" + element + "\n";
+        };
+        result +=
+            "height: " + height + "\n"
+            + "weight: " + weight + "\n"
+            + "candy: " + candy + "\n"
+            + "candy_count: " + candy_count + "\n"
+            + "egg: " + egg + "\n"
+            + "spawn_chance: " + spawn_chance + "\n"
+            + "avg_spawns: " + avg_spawns + "\n"
+            + "spawn_time: " + spawn_time + "\n"
+        + "multipliers:\n";
+
+        if (multipliers != null)
+        {
+            foreach (double element in multipliers)
+            {
+                if (element != null)
+                {
+                    result += "\t" + element + "\n";
+                }
+            };
+        }
+        result += "weaknesses:\n";
+        foreach (string element in weaknesses)
+        {
+            result += "\t" + element + "\n";
+        };
+
+        result += "prev_evolution:\n";
+        foreach (object element in prev_evolution)
+            result += "\t" + element + "\n";
+        {
+        };
+
+        result += "next_evolution:\n";
+        foreach (object element in next_evolution)
+        {
+            result += "\t" + element + "\n";
+        };
+
+        return result;
+    }
+
+
+    public record Evolution
+    {
+        public string num { get; }
+        public string name { get; }
+        public Evolution(string num, string name)
+        {
+            this.num = num;
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return "num: " + num + "\n\tname: " + name;
+        }
+    }
+}
---
+++ h050706/Program.cs
@@ -0,0 +1,240 @@
+﻿using h050706.EnumPokemon;
+using Microsoft.Win32.SafeHandles;
+using System.Collections.Specialized;
+using System.IO;
+using System.Text.Json;
+using System.Linq;
+using System.Threading.Channels;
+using System.Net.WebSockets;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Kotprog
+{
+    internal class Program
+    {
+        static PokemonCollection globalPokemons = new PokemonCollection();
+
+        private static void Main(string[] args)
+        {
+            MakeCollectionAndListPokemons(false, globalPokemons);
+            SelectMethods();
+
+            Console.WriteLine("Program vége");
+            Console.ReadKey();
+        }
+        private static void WriteInstructions()
+        {
+            Console.WriteLine("Kérlek válaszd ki mit szeretnél csinálni: ");
+            Console.WriteLine("\t 0. Kilépés");
+            Console.WriteLine("\t 1. Pokémonok listázása a megadott tojáskategória alapján");
+            Console.WriteLine("\t 2. Megadott típusú Pokémonok listázása a keltetéshez szükséges Candy alapján");
+            Console.Write("\nAdd meg a sorszámot: ");
+        }
+
+        private static void SelectMethods()
+        {
+            Console.WriteLine("Üdvözöllek a Pokémonokkal foglalkozó programomban\n");
+            WriteInstructions();
+            string selectMethod = "";
+            while (selectMethod != "0")
+            {
+                selectMethod = Console.ReadLine();
+                switch (selectMethod)
+                {
+                    case "0":
+                        break;
+                    case "1":
+                        SelectPokemonsByEggs();
+                        Console.WriteLine("---Lekérdezés vége---\n\n");
+                        WriteInstructions();
+                        break;
+                    case "2":
+                        SelectNameByTypeOrderByCandyCount();
+                        Console.WriteLine("---Lekérdezés vége---\n\n");
+                        WriteInstructions();
+                        break;
+                    default:
+                        Console.WriteLine("Helytelen sorszám, kérlek a fentiek közül válassz!");
+                        break;
+                        break;
+                }
+            }
+        }
+        private static void SelectNameByTypeOrderByCandyCount()
+        {
+            #region Creating typeList
+            List<string> typeList = OptionListing("type", true);
+            if (typeList == null)
+            {
+                Console.WriteLine("[Hiba] Nem létező opció " + nameof(SelectNameByTypeOrderByCandyCount));
+                return;
+            }
+            #endregion
+
+            #region Reading input, check if it's valid
+            string typeInput;
+            Console.WriteLine("Adja meg a szűrni kívánt típust!");
+            while (true)
+            {
+                Console.Write("Opciók: ");
+                typeList.ForEach(n => Console.Write("[" + n + "] "));
+                Console.WriteLine();
+                typeInput = Console.ReadLine();
+
+                if (typeList.Any(n => n.Equals(typeInput, StringComparison.OrdinalIgnoreCase)))
+                {
+                    break;
+                }
+                Console.WriteLine("Ismeretlen típus, válassz az alábbiakból:");
+            }
+            #endregion
+
+            #region Select the given types with not zero candy_count, sorting and grouping by candy_count and
+            var result = from pokemon in globalPokemons
+                         where pokemon.type.Any(n => n.Equals(typeInput, StringComparison.OrdinalIgnoreCase)) && pokemon.candy_count > 0
+                         orderby pokemon.candy_count ascending
+                         group pokemon by pokemon.candy_count into candy_countGroup
+                         select candy_countGroup;
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.Key + ": ");
+                foreach (var poke_item in item)
+                {
+                    Console.WriteLine("\t" + poke_item.name);
+                }
+            }
+            #endregion
+        }
+        private static void SelectPokemonsByEggs()
+        {
+            List<string> eggList = OptionListing("egg", true);
+
+            string condition;
+            while (true)
+            {
+                Console.WriteLine("Írd be az értéket: ");
+                condition = Console.ReadLine();
+
+                if (eggList.Any(n => n.Equals(condition, StringComparison.OrdinalIgnoreCase)))
+                {
+                    break;
+                }
+                Console.WriteLine("Ismeretlen típus, válassz a fenti opciók közül!");
+            }
+
+            var result = from elem in globalPokemons
+                         where elem.egg.ToLower() == condition.ToLower()
+                         select elem;
+            //Console.WriteLine(result.GetType());
+
+            if (result.Any())
+            {
+                foreach (var item in result)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+        public static List<string> OptionListing(string propertyKey, bool sorted)
+        {
+            List<string> optionNamesList = new List<string>();
+            List<string> optionList = new List<string>();
+            PropertyInfo[] properties = typeof(Pokemon).GetProperties();
+
+            Array.ForEach(properties, item => optionNamesList.Add(item.Name));
+
+            if (!optionNamesList.Any(n => n.Equals(propertyKey.ToLower(), StringComparison.OrdinalIgnoreCase)))
+            {
+                Console.WriteLine("[Error] Invalid option in " + nameof(OptionListing));
+                return null;
+            }
+
+            if (typeof(Pokemon).GetProperty(propertyKey).PropertyType.IsGenericType &&
+                typeof(IEnumerable).IsAssignableFrom(typeof(Pokemon).GetProperty(propertyKey).PropertyType))
+            {
+                foreach (var pokemon in globalPokemons)
+                {
+                    foreach (var item in (IEnumerable)pokemon.GetType().GetProperty(propertyKey).GetValue(pokemon))
+                    {
+                        if (!optionList.Contains(item))
+                        {
+                            optionList.Add(item.ToString());
+                        }
+                    }
+                }
+            }
+            else
+            {
+                var result = from pokemon in globalPokemons
+                             orderby pokemon.GetType().GetProperty(propertyKey).GetValue(pokemon) descending
+                             group pokemon by pokemon.GetType().GetProperty(propertyKey).GetValue(pokemon) into list
+                             select list;
+
+                foreach (var item in result)
+                {
+                    optionList.Add((string)item.Key);
+                }
+            }
+
+
+            if (sorted)
+            {
+                optionList.Sort();
+            }
+
+
+            Console.Write("\nOpciók: ");
+            optionList.ForEach(n => Console.Write("[" + n + "] "));
+            Console.WriteLine();
+
+            return optionList;
+        }
+        private static List<Pokemon> ReadJSON()
+        {
+            string fileName = @"pokemon.json";
+            string path = Path.Combine(AppContext.BaseDirectory, fileName);
+            List<Pokemon> pokemons = new List<Pokemon>();
+            using (FileStream file = File.OpenRead(path))
+            {
+                using (StreamReader reader = new StreamReader(file))
+                {
+                    try
+                    {
+                        pokemons = JsonSerializer.Deserialize<List<Pokemon>>(reader.ReadToEnd());
+                        return pokemons;
+                    }
+                    catch (IOException ioEx)
+                    {
+                        Console.WriteLine("Hiba keletkezett a beolvasás során: [" + ioEx.Message + "]");
+                    }
+                    catch (Exception ex) //Pokemon exception handling hehe
+                    {
+                        Console.WriteLine("Hiba keletkezett: [" + ex.Message + "]");
+                    }
+                    Console.WriteLine("Kilépéshez nyomjon meg egy gombot");
+                    Console.ReadKey();
+                    Environment.Exit(1);
+                    return null;
+                }
+            }
+        }
+        private static void MakeCollectionAndListPokemons(bool listing, PokemonCollection pokemonCollection)
+        {
+            var readedPokemons = ReadJSON();
+            foreach (var item in readedPokemons)
+            {
+                pokemonCollection.Add(item);
+            }
+
+            if (listing)
+            {
+                readedPokemons.ForEach(n => Console.WriteLine(n));
+            }
+        }
+    }
+}
