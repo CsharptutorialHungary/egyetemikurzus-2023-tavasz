265da33fc360e701c2c0e0cf912a23b049322069
Klbrtmr <kolbertmario19@gmail.com>
2023-04-14 20:40:25 +0200

    UnitTest létrehozva, egyelőre gondolkozási fázis

---
+++ ProgramTest/ProgrammingTest.cs
@@ -0,0 +1,13 @@
+using _r5jxrm_;
+
+namespace ProgramTest
+{
+    [TestClass]
+    public class ProgrammingTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
