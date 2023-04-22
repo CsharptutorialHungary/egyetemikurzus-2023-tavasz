3513b3b36a64186428aabb48bb8db7682b5e2801
Bene Martin <h043098@stud.u-szeged.hu>
2023-04-22 23:06:12 +0200

    Implement transaction and category models

---
+++ DU0038/Model/Transaction.cs
@@ -0,0 +1,19 @@
+﻿namespace DU0038.Model;
+
+public class Transaction
+{
+    public string Id { get; init; }
+    public string Name { get; set; }
+    public uint Value { get; set; }
+    public DateTime Date { get; set; }
+    public string CategoryId { get; set; }
+
+    public Transaction(string name, uint value, DateTime date, string categoryId)
+    {
+        Id = Guid.NewGuid().ToString();
+        Name = name;
+        Value = value;
+        Date = date;
+        CategoryId = categoryId;
+    }
+}
--- DU0038/Program.cs
+++ DU0038/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DU0038.Builder;
+using DU0038.Model;
+
+namespace DU0038
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
