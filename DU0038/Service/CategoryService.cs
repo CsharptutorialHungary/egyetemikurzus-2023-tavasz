52e8a7174442e958abf89a5fbd35d4a3fa2b64dc
Bene Martin <h043098@stud.u-szeged.hu>
2023-04-23 23:40:40 +0200

    Create Transaction and Category Service

---
+++ DU0038/Service/CategoryService.cs
@@ -0,0 +1,46 @@
+﻿using DU0038.Model;
+
+namespace DU0038.Service;
+
+public class CategoryService
+{
+    private static CategoryService? _instance = null;
+    private static readonly object Padlock = new object();
+    private List<Category> _categories;
+
+    public CategoryService()
+    {
+        _categories = FileService.Instance.ReadCategoriesFromFile();
+    }
+    
+    public static CategoryService Instance
+    {
+        get
+        {
+            lock (Padlock)
+            {
+                return _instance ??= new CategoryService();
+            }
+        }
+    }
+
+    public void AddCategory(string name, bool isIncome)
+    {
+        _categories.Add(new Category
+        (
+            Guid.NewGuid().ToString(),
+            name,
+            isIncome
+        ));
+    }
+
+    public void SaveCategories()
+    {
+        FileService.Instance.WriteCategoriesToFile(_categories);
+    }
+
+    public List<Category> GetCategories()
+    {
+        return _categories;
+    }
+}
---
+++ DU0038/Service/TransactionService.cs
@@ -0,0 +1,41 @@
+﻿using DU0038.Model;
+
+namespace DU0038.Service;
+
+public class TransactionService
+{
+    private static TransactionService? _instance = null;
+    private static readonly object Padlock = new object();
+    private List<Transaction> _transactions;
+
+    public TransactionService()
+    {
+        _transactions = FileService.Instance.ReadTransactionsFromFile();
+    }
+    
+    public static TransactionService Instance
+    {
+        get
+        {
+            lock (Padlock)
+            {
+                return _instance ??= new TransactionService();
+            }
+        }
+    }
+
+    public void AddTransaction(string name, uint value, DateTime date, string categoryId)
+    {
+        _transactions.Add(new Transaction(name, value, date, categoryId));
+    }
+
+    public void SaveTransactions()
+    {
+        FileService.Instance.WriteTransactionsToFile(_transactions);
+    }
+    
+    public List<Transaction> GetTransactions()
+    {
+        return _transactions;
+    }
+}
