aaf31206f5bd373083e4edfbec802ad03beec425
Bene Martin <h043098@stud.u-szeged.hu>
2023-04-24 00:52:55 +0200

    Start implementing program loop and create category add and exit functions

---
+++ DU0038/Controller/ProgramController.cs
@@ -0,0 +1,75 @@
+﻿using DU0038.Service;
+
+namespace DU0038.Controller;
+
+public class ProgramController
+{
+    public ProgramController()
+    {
+    }
+
+    public void StartProgramLoop()
+    {
+        while (true)
+        {
+            Console.WriteLine("Adjon meg egy parancsot: ");
+            string? command = Console.ReadLine();
+            if (command != null && command.Trim() != "")
+            {
+                EvaluateCommand(command);
+            }
+            else
+            {
+                Console.WriteLine("Nem adtál meg parancsot!");
+            }
+        }
+    }
+
+    private void EvaluateCommand(string command)
+    {
+        if (command == "add category")
+        {
+            HandleAddCategory();
+        }
+        else if (command == "exit")
+        {
+            SaveStateAndExitProgram();
+        }
+    }
+
+    private void HandleAddCategory()
+    {
+        string? categoryName = null;
+        string? isIncomeString = null;
+
+        while (categoryName == null || categoryName.Trim() == "")
+        {
+            Console.WriteLine("Adjon meg egy nevet a kategóriának:");
+            categoryName = Console.ReadLine();
+            if (categoryName == null || categoryName.Trim() == "")
+            {
+                Console.WriteLine("Érvénytelen érték!");
+            }
+        }
+        
+        while (isIncomeString == null || isIncomeString.Trim() == "" || (isIncomeString != "+" && isIncomeString != "-"))
+        {
+            Console.WriteLine("Bevétel (+) vagy költség (-) kategória lesz:");
+            isIncomeString = Console.ReadLine();
+            if (isIncomeString == null || isIncomeString.Trim() == "" || (isIncomeString != "+" && isIncomeString != "-"))
+            {
+                Console.WriteLine("Érvénytelen érték! Adjon meg + vagy - karaktert!");
+            }
+        }
+        
+        CategoryService.Instance.AddCategory(categoryName, isIncomeString == "+");
+        Console.WriteLine("Kategória hozzáadása sikeres!");
+    }
+
+    private void SaveStateAndExitProgram()
+    {
+        CategoryService.Instance.SaveCategories();
+        TransactionService.Instance.SaveTransactions();
+        Environment.Exit(0);
+    }
+}
--- DU0038/Program.cs
+++ DU0038/Program.cs
@@ -1,5 +1,5 @@
-﻿using DU0038.Builder;
-using DU0038.Model;
+﻿using DU0038.Controller;
+using DU0038.Service;
 
 namespace DU0038
 {
@@ -7,7 +7,16 @@ namespace DU0038
     {
         public static void Main(string[] args)
         {
+            ProgramController programController = new ProgramController();
+            programController.StartProgramLoop();
 
+            //Kategória hozzáadása
+            //Egyenleg lekérése
+            //Bevétel Kategóriánként
+            //Költségek kategóriánként
+            //Tranzakciók listázása
+            //Tranzakciók kategóriánként
+            //Tranzakció hozzáadása
         }
     }
 }
