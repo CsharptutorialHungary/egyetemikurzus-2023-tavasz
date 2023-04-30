df6aaf848a00e7b9c7eee1daa1f5803e2c6d3752
LeviSooky <levente.zsolt.sooky@gmail.com>
2023-04-30 14:52:09 +0200

    Add homework

---
+++ N6L972/Hero.cs
@@ -0,0 +1,20 @@
+﻿namespace N6L972;
+
+public record Hero
+{
+    public string HeroName { get; set; }
+    public string RealName { get; set; }
+    public string SuperPower { get; set; }
+    public int DuckStrength { get; set;  }
+
+    public static Hero BuildFromFileLine(string[] fields)
+    {
+        return new Hero()
+        {
+            HeroName = fields[0],
+            RealName = fields[1],
+            SuperPower = fields[2],
+            DuckStrength = int.Parse(fields[3])
+        };
+    }
+}
