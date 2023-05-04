1687f6ba3fbc0eb01515de8c9949fdbbcd6f42dd
Pistifeju <juhasz.pisti7253@gmail.com>
2023-05-04 19:16:46 +0200

    Finished ApiDataProvider singleton

--- T4XJYT-LGI301/T4XJYT-LGI301/Core/API/ApiDataProvider.cs
+++ T4XJYT-LGI301/T4XJYT-LGI301/Core/API/ApiDataProvider.cs
@@ -5,18 +5,58 @@ namespace T4XJYT_LGI301.Core.API
 {
     public class ApiDataProvider : IApiDataProvider
     {
-        private readonly HttpClient _httpClient;
+        private static ApiDataProvider _instance;
+
+        private readonly HttpClient _httpClient = new HttpClient();
         private readonly string _apiURL = "https://baconipsum.com/api/?type=meat-and-filler";
 
-        public ApiDataProvider(HttpClient httpClient)
+        private ApiDataProvider() {}
+
+        public static ApiDataProvider GetInstance()
         {
-            _httpClient = httpClient;
+            if (_instance == null)
+                _instance = new ApiDataProvider();
+
+            return _instance;
         }
 
-        public Task<string> GetTextFromAPI()
+        public async Task<string> GetTextFromAPI()
         {
-            // TODO:
-            throw new NotImplementedException();
+            try
+            {
+                using HttpClient httpClient = new HttpClient();
+                HttpResponseMessage response = await httpClient.GetAsync(_apiURL);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string text = await response.Content.ReadAsStringAsync();
+                    return text;
+                }
+                else
+                {
+                    // Handle unsuccessful status codes
+                    string errorMessage = $"Error: {response.StatusCode} - {response.ReasonPhrase}";
+                    throw new HttpRequestException(errorMessage);
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                // Handle HttpRequestException - this can occur due to unsuccessful status codes or other request-related issues
+                Console.WriteLine($"Request error: {ex.Message}");
+                return string.Empty;
+            }
+            catch (TaskCanceledException ex)
+            {
+                // Handle TaskCanceledException - this can occur due to a request timeout or cancellation
+                Console.WriteLine($"Request timeout or canceled: {ex.Message}");
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                // Handle any other unhandled exceptions
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+                return string.Empty;
+            }
         }
     }
 }
--- T4XJYT-LGI301/T4XJYT-LGI301/Core/Program.cs
+++ T4XJYT-LGI301/T4XJYT-LGI301/Core/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!!!!");
+﻿using T4XJYT_LGI301.Core.API;
+
+namespace T4XJYT_LGI301
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Getting the text from the API...\n\n");
+            ApiDataProvider apiDataProvider = ApiDataProvider.GetInstance();
+            string textFromApi = await apiDataProvider.GetTextFromAPI();
+        }
+    }
+}
