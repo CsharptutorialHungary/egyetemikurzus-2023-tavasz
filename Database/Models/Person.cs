a84d36a87e40c6ead739d08c4c5fe33af49549a0
Fazekas László <fazekas@inf.u-szeged.hu>
2023-05-07 13:00:12 +0200

    Db project init

---
+++ Database/Models/Person.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Database.Models;
+public class Person : BaseModel
+{
+
+}
---
+++ Database/PeopleContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database;
+public class PeopleContext: DbContext
+{
+
+}
