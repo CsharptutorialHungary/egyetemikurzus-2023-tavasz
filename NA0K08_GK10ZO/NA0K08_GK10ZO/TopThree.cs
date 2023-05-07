c3de7f6200655b86e07723ede92ef15eaca5d6e5
Kecskés <h046702@stud.u-szeged.hu>
2023-05-07 22:08:07 +0200

    Created Immutable TopThree class

---
+++ NA0K08_GK10ZO/NA0K08_GK10ZO/TopThree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NA0K08_GK10ZO
+{
+    internal class TopThree
+    {
+        readonly private string first;
+        readonly private string second;
+        readonly private string third;
+
+        public TopThree(string first, string second, string third)
+        {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+
+        public string getFirst() { return first; }
+
+        public string getSecond() { return second; }
+
+        public string getThird() { return third; }
+
+    }
+}
