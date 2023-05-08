b0f96a82dd0ad7059d6cb8011b7e10ecdce4d5fd
Fazekas László <fazekas@inf.u-szeged.hu>
2023-05-08 12:28:24 +0200

    added statistics

--- D9MXN2/DataAccess/Actions/Statistics.cs
+++ D9MXN2/DataAccess/Actions/Statistics.cs
@@ -1,22 +1,32 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using D9MXN2.Models;
+using Database;
 using Database.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace D9MXN2.DataAccess.Actions;
-public class Statistics
+public static class Statistics
 {
-    PersonContainer<Person> container = new();
+    static PersonContainer<Person> _container = new();
 
-    public static void PrintMax()
+    public static void PrintStatistics()
     {
-        
-    }
+        using (var db = SqliteDatabaseFactory<PeopleContext>.Create())
+        {
+            foreach (var person in db.People.Include(p => p.Notes))
+            {
+                _container.Add(person);
+            }
 
-    public static void PrintMin()
-    {
+            var ordered_people_with_index = _container
+                .OrderBy(p => p.Notes.Count)
+                .Zip(Enumerable.Range(0, _container.Count));
 
+            foreach ((Person elem, int index) in ordered_people_with_index)
+            {
+                Console.WriteLine($"{index+1}: {elem.Username} with {elem.Notes.Count} notes.");
+            }
+        }
     }
 }
--- D9MXN2/UI/App.cs
+++ D9MXN2/UI/App.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using D9MXN2.DataAccess.Actions;
 
 namespace D9MXN2.UI;
 
@@ -10,6 +11,7 @@ public class App : CommandScreen
     protected override Dictionary<string, Delegate> Commands { get; set; } = new() {
         {"login", () => ScreenRender(new LoginScreen())},
         {"register", () => ScreenRender(new RegisterScreen())},
+        {"statistics", () => Statistics.PrintStatistics()},
         {"help", () => Help()},
         {"exit", () => Environment.Exit(0)}
     };
@@ -21,6 +23,7 @@ public class App : CommandScreen
         Console.WriteLine("Commands:");
         Console.WriteLine("login: You can login here if you are registered.");
         Console.WriteLine("register: You can register here.");
+        Console.WriteLine("statistics: Print statistics about the users.");
         Console.WriteLine("exit: closes the application.");
         Console.WriteLine();
         Console.WriteLine("Press anything to proceed.");
@@ -41,7 +44,6 @@ public class HomeScreen : CommandScreen
         {"notes", () => System.Console.WriteLine()},
         {"add", () => System.Console.WriteLine()},
         {"delete", () => System.Console.WriteLine()},
-        {"statistics", () => System.Console.WriteLine()},
         {"dump", () => System.Console.WriteLine()},
         {"load", () => System.Console.WriteLine()},
         {LOGOUT_COMMAND, () => Console.WriteLine("Goodbye")},
